fb310f407dbc70147864d64edcbbca9b564ec6e4
Phillip Rascher <47728570+prascher@users.noreply.github.com>
2024-12-19 17:17:42 -0600

    refactor database connection to use environment variables

--- conn.php
+++ conn.php
@@ -1,6 +1,6 @@
 <?php
-$host = 'localhost'; // this is the database hostname that has your backing database.
-$user = 'library'; // use a username that can access the database you are using.
-$pass = 'password'; // use password you made up.
-$db = 'library'; // use the name of database you are using.
+$host = getenv('DB_HOST');
+$user = getenv('DB_USER');
+$pass = getenv('DB_PASS');
+$db = getenv('DB_NAME');
 ?>
